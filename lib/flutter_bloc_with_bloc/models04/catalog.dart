b496b8f93ae13043ce0833ecc2929922be165d2c
Lavitskaya Victoria <nika10by@mail.ru>
2022-06-22 11:45:31 +0300

    add "Bloc"

--- lib/flutter_bloc_with_bloc/models04/catalog.dart
+++ lib/flutter_bloc_with_bloc/models04/catalog.dart
@@ -4,11 +4,11 @@ import '../../models/product.dart';
 abstract class TotalAmountEvent {}
 class TotalAmountFind extends TotalAmountEvent {}
 
-class Catalog extends Bloc<TotalAmountEvent, double> {
+class CatalogBloc extends Bloc<TotalAmountEvent, double> {
 
   late Map<Product, int> listProduct;
 
-  Catalog() : super(0) {
+  CatalogBloc() : super(0) {
     on<TotalAmountFind>((event, emit) {
       emit(0);
       listProduct.forEach((product, amount) {
@@ -27,13 +27,4 @@ class Catalog extends Bloc<TotalAmountEvent, double> {
       listProduct[product] = (listProduct[product] ?? 0) - 1;
     }
   }
-
-  // void findTotalAmount() {
-  //   emit(0);
-  //   listProduct.forEach((product, amount) {
-  //     emit(state + product.cost * amount);
-  //   });
-  // }
-
 }
-
